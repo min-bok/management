b24e951a9909a3b51b945105c6715d423da14a23
HyeyeongLee <hylee@entcrowd.com>
2022-08-04 13:57:25 +0900

    Create Customer.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,50 @@
 import logo from "./logo.svg";
 import "./App.css";
+import Customer from "./components/Customer";
+
+const customers = [
+  {
+    id: 1,
+    image: "https://placeimg.com/64/64/1",
+    name: "나동빈",
+    birthday: "961222",
+    gender: "남자",
+    job: "대학생",
+  },
+  {
+    id: 2,
+    image: "https://placeimg.com/64/64/2",
+    name: "이혜영",
+    birthday: "980810",
+    gender: "여자",
+    job: "대학생",
+  },
+  {
+    id: 3,
+    image: "https://placeimg.com/64/64/3",
+    name: "김범진",
+    birthday: "970730",
+    gender: "남자",
+    job: "대학생",
+  },
+];
 
 function App() {
   return (
-    <div className="gray-background">
-      <img src={logo} alt="logo" />
-      <h2>Let's dev management system!</h2>
+    <div>
+      {customers.map((customer) => {
+        return (
+          <Customer
+            key={customer.id}
+            id={customer.id}
+            image={customer.image}
+            name={customer.name}
+            birthday={customer.birthday}
+            gender={customer.gender}
+            job={customer.job}
+          />
+        );
+      })}
     </div>
   );
 }
