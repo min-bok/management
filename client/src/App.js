40139473335c1e3792206fe7304307c2f5a49a50
HyeyeongLee <hylee@entcrowd.com>
2022-08-07 18:48:15 +0900

    done except entire mui ui design

--- client/src/App.js
+++ client/src/App.js
@@ -12,6 +12,16 @@ import { CircularProgress } from "@mui/material";
 import axios from "axios";
 import CustomerAdd from "./components/CustomerAdd";
 
+// import { styled, alpha } from "@mui/material/styles";
+// import AppBar from "@mui/material/AppBar";
+// import Box from "@mui/material/Box";
+// import Toolbar from "@mui/material/Toolbar";
+// import IconButton from "@mui/material/IconButton";
+// import Typography from "@mui/material/Typography";
+// import InputBase from "@mui/material/InputBase";
+// import MenuIcon from "@mui/icons-material/Menu";
+// import SearchIcon from "@mui/icons-material/Search";
+
 const styles = {
   root: {
     width: "100%",
